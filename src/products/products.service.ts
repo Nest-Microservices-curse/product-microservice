7aad96362b515757a433d261e139e8a89e24db5b
Daniel <dmiramontesg@gmail.com>
2024-04-21 19:19:47 -0600

    product microservice

---
+++ src/products/products.service.ts
@@ -0,0 +1,84 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Database connected');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto
+    });
+  }
+
+  async findAll(pagintionDto) {
+
+    const { page, limit } = pagintionDto;
+
+    const totalPages = await this.product.count({ where: { available: true }});
+
+    const lastpage = Math.ceil(totalPages / limit);
+    return {
+      data : await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: {
+          available: true
+        }
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastpage: lastpage
+      }
+    }
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, available:true }
+    });
+
+    if(! product){
+      throw new NotFoundException(`Product with id ${ id } not found`);
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const {id:__ , ...data } = updateProductDto;
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id},
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+
+    await this.findOne(id);
+
+    // return this.product.delete({
+    //   where: { id }
+    // });
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false
+      }
+    });
+
+    return product
+
+
+  }
+}
